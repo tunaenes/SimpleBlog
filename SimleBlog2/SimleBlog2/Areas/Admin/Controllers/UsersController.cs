ee5981ad8d1aa50d54a6d564c3de23d2b4d92564
tunaenes <tunideneme53@gmail.com>
2017-02-23 17:53:48 +0200

    23.02.2017 commit

---
+++ SimleBlog2/SimleBlog2/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimleBlog2.Areas.Admin.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Admin/Users
+        public ActionResult Index()
+        {
+            return Content("Index Screen");
+        }
+        public ActionResult List()
+        {
+            return Content("List Screen");
+        }
+        public ActionResult Edit(int id=0)
+        {
+            return Content("Edit Screen"+id.ToString());
+        }
+    }
+}
